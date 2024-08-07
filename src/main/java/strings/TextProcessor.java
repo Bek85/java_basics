483a03cc7e968a425fbd17bcf9af592e7413e2c4
Bekzod Akbarov <stx.webdev@gmail.com>
2024-08-07 17:48:31 +0500

    String & Text processing

---
+++ src/main/java/strings/TextProcessor.java
@@ -0,0 +1,19 @@
+package strings;
+
+public class TextProcessor {
+    public static void main(String[] args) {
+        countWords("I love Java");
+    }
+
+    public static void countWords(String text) {
+        var words = text.split(" ");
+        int numberOfWords = words.length;
+
+        String message = String.format("Your text contains %d words", numberOfWords);
+        System.out.println(message);
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
