576aea352f6e1bd79409c4b82aa08fb04b0cb3b2
Bekzod Akbarov <stx.webdev@gmail.com>
2024-08-08 19:23:12 +0500

    inheritance in practice

---
+++ src/main/java/inheritance/InheritanceTester.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class InheritanceTester {
+    public static void main(String[] args) {
+//        Person person = new Person();
+        Employee employee = new Employee();
+    }
+}
