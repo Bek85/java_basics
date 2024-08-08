ebaba7f9069a3df9b5b8c4313b7991d21dee48bf
Bekzod Akbarov <stx.webdev@gmail.com>
2024-08-08 18:57:59 +0500

    Jumbled String

--- src/main/java/strings/TextProcessor.java
+++ src/main/java/strings/TextProcessor.java
@@ -3,7 +3,20 @@ package strings;
 public class TextProcessor {
     public static void main(String[] args) {
 //        countWords("I love Java");
-        reverseString("Hello World");
+//        reverseString("Hello World");
+        addSpaces("HeyEverybody!How'sItGoing?");
+    }
+
+    private static void addSpaces(String text) {
+        var modifiedText = new StringBuilder(text);
+
+        for (int i = 0; i < modifiedText.length(); i++) {
+            if(i != 0 && Character.isUpperCase(modifiedText.charAt(i))) {
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+        System.out.println(modifiedText);
     }
 
     private static void reverseString(String text) {
